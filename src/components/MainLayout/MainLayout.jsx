8025d794d3d4e5e68538a7d4dbb64e31c33ad353
Filip Szymala <filipszymala@gmail.com>
2023-03-14 21:41:56 +0100

    Added hamburger working hamburer menu, fixed some smaller screens bugs. MainLayout still needs a bit of refactor and clean-up

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,47 +1,75 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/ZSP-logo.svg";
 import "./MainLayout.css";
 import { Outlet } from "react-router-dom";
+import { React, useState } from "react";
 
-const TopBar = () => (
-  <>
-    <div className="top-bar">
-      <header className="top-bar__header container">
-        <Link to="/" rel="noreferrer" className="logo">
-          <img src={logo} />
-        </Link>
-        <div className="contact-info">
-          <span className="contact-info__phone-label">
-            skontaktuj się z nami:
-          </span>
-          <a href="tel:000000000" className="contact-info__phone-link">
-            000-000-000
-          </a>
-        </div>
-      </header>
-      <nav className="top-bar__navigation ">
-        <div className="navigation__links container">
-          <Link to="/" rel="noreferrer" className="links__link btn active-link">
-            strona główna
-          </Link>
-          <Link to="/o_nas" rel="noreferrer" className="links__link btn">
-            o nas
-          </Link>
-          <Link to="/oferta" rel="noreferrer" className="links__link btn">
-            oferta
-          </Link>
-          <Link to="/akutalnosci" rel="noreferrer" className="links__link btn">
-            aktualności
-          </Link>
-          <Link to="/kontakt" rel="noreferrer" className="links__link btn">
-            kontakt
-          </Link>
-        </div>
-      </nav>
-    </div>
-    <Outlet />
-  </>
-);
+const TopBar = () => {
+  const [hamburger, setHamburger] = useState(false);
+  const openHamburger = () => {
+    setHamburger(!hamburger);
+  };
+  return (
+    <>
+      <div className="top-bar">
+        <header className="top-bar__header-wrapper">
+          <div
+            className={hamburger ? "hamburger open" : "hamburger"}
+            onClick={() => openHamburger()}
+          >
+            <div />
+            <div />
+            <div />
+          </div>
+          <div className="top-bar__header container">
+            <Link to="/" rel="noreferrer" className="logo">
+              <img src={logo} />
+            </Link>
+            <div className="contact-info">
+              <span className="contact-info__phone-label">
+                skontaktuj się z nami:
+              </span>
+              <a href="tel:000000000" className="contact-info__phone-link">
+                000-000-000
+              </a>
+            </div>
+          </div>
+        </header>
+        <nav
+          className={
+            hamburger ? "top-bar__navigation open" : "top-bar__navigation"
+          }
+        >
+          <div className="navigation__links container">
+            <Link
+              to="/"
+              rel="noreferrer"
+              className="links__link btn active-link"
+            >
+              strona główna
+            </Link>
+            <Link to="/o_nas" rel="noreferrer" className="links__link btn">
+              o nas
+            </Link>
+            <Link to="/oferta" rel="noreferrer" className="links__link btn">
+              oferta
+            </Link>
+            <Link
+              to="/akutalnosci"
+              rel="noreferrer"
+              className="links__link btn"
+            >
+              aktualności
+            </Link>
+            <Link to="/kontakt" rel="noreferrer" className="links__link btn">
+              kontakt
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <Outlet />
+    </>
+  );
+};
 
 export default TopBar;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./MainPage.css";
 const MainPage = () => {
-  return <p>MainPage</p>;
+  return (
+    <main>
+      <img src="http://zooart.com.pl/blog/wp-content/uploads/2020/06/kapibara-w-domu.jpeg"></img>
+    </main>
+  );
 };
 
 export default MainPage;
